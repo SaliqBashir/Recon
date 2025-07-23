286085728a92c50f1ed9d1d5d30fade4b18ccb6d
Saliq Bashir <salikbhat541@gmail.com>
2025-07-23 14:19:37 +0530

    Made the basic bot commands

---
+++ app.py
@@ -0,0 +1,19 @@
+import discord
+import os # default module
+import dotenv
+from dotenv import load_dotenv
+
+dotenv.load_dotenv()
+token = str(os.getenv("TOKEN"))
+load_dotenv() # load all the variables from the env file
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+@bot.slash_command(name="hello", description="Say hello to the bot")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hey!")
+
+bot.run(os.getenv('TOKEN')) # run the bot with the token
